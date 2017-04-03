c5881f2a6b6e85166395d1765a28a839e87d3917
OliviaZeng <zzmickey211@163.com>
2017-04-03 10:32:02 +0800

    vector and map

--- list/virtual.cpp
+++ list/virtual.cpp
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
+#include <vector>
+#include <map>
 
 
 using namespace std;
@@ -51,31 +53,37 @@ int main(int argc, char *argv[])
     delete base2;
     std::cout<<"-------------------------------"<<std::endl;
     delete child;
+////////////////////////////////////////////////////////////
 
+
+    map<int, int> m ;
+    m.insert(pair<int, int>(1, 1)) ;
+    m.insert(pair<int, int>(2, 2)) ;
+    m.insert(pair<int, int>(3, 3)) ;
+    m.insert(pair<int, int>(4, 3)) ;
+    m.insert(pair<int, int>(5, 5)) ;
+	for (map<int,int>::iterator iter=m.begin();iter!=m.end();) {
+		if (3 == iter->second)
+			m.erase(iter++);
+		else
+			iter++;
+	}
+
+	vector<int> it;
+	it.push_back(1);
+	it.push_back(2);
+	it.push_back(3);
+	it.push_back(3);
+	it.push_back(5);
+	it.push_back(6);
+	it.erase(remove(it.begin(), it.end(),3), it.end());
+
+	for (vector<int>::iterator iter=it.begin();iter!=it.end();iter++) {
+		printf("%d ", *iter);
+	}
+	printf("\n");
+	for (map<int,int>::iterator iter=m.begin();iter!=m.end();iter++) {
+		printf("%d-%d ", iter->first,iter->second);
+	}
     return 0;
 }
-#if 0
-Base::Base()
--------------------------------
-Base::Base()
-Children::Children()
--------------------------------
-Base::Base()
-Children::Children()
--------------------------------
--------------------------------
-Base::Printf
-Children::Printf
-Children::Printf
--------------------------------
-Base::~Base()
--------------------------------
-Children::~Children()
-Base::~Base()
--------------------------------
-Children::~Children()
-Base::~Base()
-#endif
-
-
-
