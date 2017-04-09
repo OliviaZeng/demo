18f43786fe15617510ce2d12aec902882fbad1eb
OliviaZeng <zzmickey211@163.com>
2017-04-09 11:24:38 +0800

    update rslist.c

--- list/rslist.c
+++ list/rslist.c
@@ -4,74 +4,42 @@
 typedef struct node{
     int data;
     struct node *next;
-}Node;
+}Node, List;
 
-Node *head = NULL;
-
-Node * create(int data)
+List *init(void)
 {
     Node *node = (Node *)malloc(sizeof(Node));
     if (NULL == node)
         return NULL;
-    node->data = data;
-    node->next = NULL;
-    return  node;
+
+    node->data = 0;
+    node->next = node;
+    return node;
 }
 
-Node * create_head(int data)
+
+void print(List *list)
 {
-    if (NULL == (head = create(data)))
-        return NULL;
-
-    head->next = head;
-    return head;
-}
-
-void add(int n)
-{
-    int i;
-    Node *tail = NULL;
-    Node *node = NULL;
-    if (NULL == head) {
-        create_head(0);
-        exit(1);
-    }
-    tail = head;
-
-    for (i=0; i<n; i++) {
-        node = (Node *)malloc(sizeof(Node));
-        if (NULL == node)
-            exit(1);
-
-        node->data = rand() % 100;
-        node->next = head;
-        tail->next = node;
-        tail = node;
-    }
-}
-
-void print(void)
-{
-    if (NULL == head)
+    if (NULL == list)
         return;
 
-    Node *p = head->next;
-    while (p && p != head) {
+    Node *p = list->next;
+    while (p && p != list) {
         printf("%d ", p->data);
         p = p->next;
     }
     printf("\n");
 }
 
-int length(void)
+int length(List *list)
 {
     int len = 0;
     Node *p = NULL;
-    if (NULL == head)
+    if (NULL == list)
         return -1;
 
-    p = head->next;
-    while (p && p != head) {
+    p = list->next;
+    while (p && p !=list) {
         ++len;
         p = p->next;
     }
@@ -79,17 +47,19 @@ int length(void)
     return len;
 }
 
-int insert(int pos, int data)
+int insert(List *list, int pos, int data)
 {
     int len = 0;
-    if (-1 == (len=length()))
+    if (-1 == (len=length(list)))
         return -1;
 
     if (pos > 0 && pos <= len+1) {
-        Node *tmp = head;
-        Node *node = create(data);
+        Node *tmp = list;
+        Node *node = (Node *)malloc(sizeof(Node));
         if (NULL == node)
             return -1;
+        node->data = data;
+        node->next = NULL;
 
         while (1) {
             pos--;
@@ -106,15 +76,15 @@ int insert(int pos, int data)
     }
 }
 
-int delete(int pos)
+int delete(List *list, int pos)
 {
     int len = 0;
     Node *next = NULL;
     Node *pre = NULL;
-    if (-1 == (len=length()))
+    if (-1 == (len=length(list)))
         return -1;
 
-    pre = head;
+    pre = list;
     if (pos > 0 && pos < len+1) {
         while (1) {
             pos--;
@@ -132,14 +102,14 @@ int delete(int pos)
     
 }
 
-int find(int pos, int *data)
+int find(List *list, int pos, int *data)
 {
     int len = 0;
-    if (-1 == (len=length()))
+    if (-1 == (len=length(list)))
         return -1;
 
     if (pos > 0 && pos <= len) {
-        Node *cur = head;
+        Node *cur = list;
         while (pos--) {
             cur = cur->next;
         }
@@ -150,14 +120,14 @@ int find(int pos, int *data)
     }
 }
 
-int modify(int pos, int data)
+int modify(List *list, int pos, int data)
 {
     int len = 0;
-    if (-1 == (len=length()))
+    if (-1 == (len=length(list)))
         return -1;
 
     if (pos > 0 && pos <= len) {
-        Node *cur = head;
+        Node *cur = list;
         while (pos--)
             cur = cur->next;
         cur->data = data;
@@ -171,18 +141,24 @@ int modify(int pos, int data)
 int main(int argc, char *argv[])
 {
     int i = 0, data = 0;
-    if (NULL == create_head(0)) {
+    List *list = init();
+    if (NULL == list) {
         printf("create head err\n");
         exit(1);
     }
 
-    add(10);
-    print();
-    modify(5,123);
-    print();
-    if (-1 != (i=find(4, &data)))
+    insert(list, 1, 1);
+    insert(list, 1, 2);
+    insert(list, 1, 3);
+    insert(list, 1, 4);
+    insert(list, 1, 5);
+    insert(list, 1, 6);
+    print(list);
+    modify(list, 5,123);
+    print(list);
+    if (-1 != (i=find(list, 4, &data)))
         printf("i=%d\n", data);
-    delete(3);
-    print();
+    delete(list, 3);
+    print(list);
     return 0;
 }
